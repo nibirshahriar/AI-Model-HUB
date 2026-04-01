0eca3aece9b6fdb76c8b7b63c9c39f8e01202ee5
nibirshahriar <nibir1891@gmail.com>
2026-04-01 20:03:49 +0600

    added navbar and banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
  import './App.css'
+import Banner from './components/Banner'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
  <Navbar/>
+<Banner/>
     </>
   )
 }
